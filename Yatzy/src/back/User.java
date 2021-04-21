fb83fda8b84fc223348b8fdfddca00a99cca8cee
ThomasJRye <thomasjrye@gmail.com>
2021-04-21 11:09:05 +0200

    Update User.java

--- Yatzy/src/back/User.java
+++ Yatzy/src/back/User.java
@@ -1,5 +1,42 @@
 package back;
 
 public class User {
+	public String username;
+	public String password;
+	public String navn;
+	public String epost;
 
+	public User(String username, String password, String navn, String epost) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.navn = navn;
+		this.epost = epost;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+	public String getEpost() {
+		return epost;
+	}
+	public void setEpost(String epost) {
+		this.epost = epost;
+	}
 }
